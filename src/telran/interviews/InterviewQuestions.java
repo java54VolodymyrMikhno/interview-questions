7cdef7702ec116d1e296fe265a89420b1693eae0
VolodymyrMikhno <volodymyr3583@gmail.com>
2024-06-28 17:16:48 +0300

    Interview questions

--- src/telran/interviews/InterviewQuestions.java
+++ src/telran/interviews/InterviewQuestions.java
@@ -9,42 +9,52 @@ public class InterviewQuestions {
 		displayOccurrences(treeMapOccurrences);
 	}
 
-	
-
 	private static TreeMap<Integer, TreeSet<String>> getTreeOccurences(HashMap<String, Integer> mapOccurences) {
-		TreeMap<Integer,TreeSet<String>> result = new TreeMap<Integer, TreeSet<String>>(Comparator.reverseOrder());
+		TreeMap<Integer, TreeSet<String>> result = new TreeMap<Integer, TreeSet<String>>(Comparator.reverseOrder());
 		mapOccurences.entrySet()
-		.forEach(e->result.computeIfAbsent(e.getValue(),k->new TreeSet<>()).add(e.getKey()));
+				.forEach(e -> result.computeIfAbsent(e.getValue(), k -> new TreeSet<>()).add(e.getKey()));
 		return result;
 	}
 
-
-
-	private static HashMap<String, Integer> getOccurrencesMap(String[]strings) {
-		HashMap<String,Integer> result = new HashMap<>();
-		for(String str :strings) {
+	private static HashMap<String, Integer> getOccurrencesMap(String[] strings) {
+		HashMap<String, Integer> result = new HashMap<>();
+		for (String str : strings) {
 			result.merge(str, 1, Integer::sum);
 		}
 		return result;
-		
+
 	}
 
 	public static void displayOccurrences(TreeMap<Integer, TreeSet<String>> treeMapOccurrences) {
 		treeMapOccurrences.entrySet().forEach(e -> {
-			e.getValue().forEach(str-> System.out.printf("%s => %d\n",str,e.getKey()));
+			e.getValue().forEach(str -> System.out.printf("%s => %d\n", str, e.getKey()));
 		});
 
 	}
-	static public boolean isSum2(int [] array, int sum) {
-		//TODO returns true if a given array contains two numbers, the summing of which
-		//equals a given 'sum' value
-		//complexity O[N] only one pass over the elements
-		return false;
+
+	static public boolean isSum2(int[] array, int sum) {
+		Set<Integer> numbers = new HashSet<>();
+		boolean res = false;
+		for (int num : array) {
+			if (numbers.contains(sum - num)) {
+				res = true;
+			}
+			numbers.add(num);
+		}
+		return res;
 	}
-	static public int getMaxWithNegativePresentation(int [] array) {
-		//TODO returns maximal positive value for which exists negative one with the same abs value
-		//if no pair of positive and negative values with the same abs value the method returns -1
-		//complexity O[N] only one pass over the elements
-		return -1;
+
+	static public int getMaxWithNegativePresentation(int[] array) {
+		Set<Integer> numbers = new HashSet<>();
+		int maxValue = -1;
+
+		for (int num : array) {
+			if (num > 0 && numbers.contains(-num)) {
+				maxValue = Math.max(maxValue, num);
+			} else if (num < 0) {
+				numbers.add(num);
+			}
+		}
+		return maxValue;
 	}
 }
